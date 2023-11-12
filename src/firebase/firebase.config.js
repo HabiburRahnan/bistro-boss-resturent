ca1fa8bc644556cd6a6f2debdf936f88790f10ce
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-11-13 00:13:31 +0600

    install firebase and use firebase login authenication code set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDX5v6QeLmhyxplnNa4lCe_xMqFqq4tjBc",
+  authDomain: "bistro-boss-95277.firebaseapp.com",
+  projectId: "bistro-boss-95277",
+  storageBucket: "bistro-boss-95277.appspot.com",
+  messagingSenderId: "1038365193852",
+  appId: "1:1038365193852:web:6ec3fcf4dbcaa646fbbbf9",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
