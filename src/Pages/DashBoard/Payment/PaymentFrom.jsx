58f6644ee9f272a0b871e79b4d9aea199eb4ccb8
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-11-20 02:01:25 +0600

    stripe thaki payment method and paymen tintent shand save id and name and payment histry

---
+++ src/Pages/DashBoard/Payment/PaymentFrom.jsx
@@ -0,0 +1,103 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
+import UseAuth from "../../../hooks/UseAuth";
+
+const PaymentFrom = () => {
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const { user } = UseAuth();
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    axiosSecure
+      .post("/create-payment-intent", { price: totalPrice })
+      .then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      });
+  }, [totalPrice, axiosSecure]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment Error", error);
+      setError(error.message);
+    } else {
+      console.log("paymentMethod", paymentMethod);
+      setError("");
+    }
+
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log(confirmError);
+    }
+    if (paymentIntent.status === "succeeded") {
+      setTransactionId(paymentIntent.id);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}></CardElement>
+      <button
+        className="btn btn-outline bg-orange-400 my-10 "
+        type="submit"
+        disabled={!stripe || !clientSecret}>
+        Pay
+      </button>
+      <p className="text-red-600 text-lg">{error}</p>
+      {transactionId && (
+        <p className="text-blue-600 text-lg">
+          Your transaction Id: {transactionId}
+        </p>
+      )}
+    </form>
+  );
+};
+
+export default PaymentFrom;
